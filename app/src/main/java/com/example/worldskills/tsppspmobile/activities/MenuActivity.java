643f5a4ad5697980813997b8b48b79a28bed9f2f
sydevelopers <sydevelopers2018@gmail.com>
2018-09-01 13:15:57 -0500

    first

--- app/src/main/java/com/example/worldskills/tsppspmobile/activities/MenuActivity.java
+++ app/src/main/java/com/example/worldskills/tsppspmobile/activities/MenuActivity.java
@@ -1,17 +1,41 @@
 package com.example.worldskills.tsppspmobile.activities;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 import com.example.worldskills.tsppspmobile.R;
 
 public class MenuActivity extends AppCompatActivity {
 
-
+    int id;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
 
+        Bundle bundle=getIntent().getExtras();
+        if (bundle!=null){
+             id=bundle.getInt("codigo");
+        }
+
+    }
+
+    public void next(View view) {
+        Intent intent=null;
+        switch (view.getId()){
+            case R.id.btn_time_log:
+                intent=new Intent(this,TimeLogActivity.class);
+                break;
+            case R.id.btn_defect_log:
+                intent=new Intent(this,DefectLogActivity.class);
+                break;
+            case R.id.btn_sumary:
+                intent=new Intent(this,ProjectPlanSummaryActivity.class);
+                break;
+        }
+        intent.putExtra("id",id);
+        startActivity(intent);
     }
 }
